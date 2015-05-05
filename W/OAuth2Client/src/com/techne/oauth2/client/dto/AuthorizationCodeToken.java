28a0c074f2fd4bbd5808e367a9a7642fa0b7660e
humbertodias <humberto.dias@techne.com.br>
2015-05-05 12:59:00 -0300

    Removendo warnings

--- W/OAuth2Client/src/com/techne/oauth2/client/dto/AuthorizationCodeToken.java
+++ W/OAuth2Client/src/com/techne/oauth2/client/dto/AuthorizationCodeToken.java
@@ -1,16 +1,26 @@
 package com.techne.oauth2.client.dto;
 
 public class AuthorizationCodeToken {
-	//request
-	private String scope;
-	private String state;
-	private String redirect_uri;//Determines where the response is sent
-	private String response_type="code";//Indicates Authorization code
-	private String client_id;
-	private String approval_prompt;
-	private String client_secret;
-	private String grant_type="authorization_code";
-	
-	//response
-	private String code;
+    //request
+    private String scope;
+    private String state;
+    private String redirect_uri;//Determines where the response is sent
+    private String response_type="code";//Indicates Authorization code
+    private String client_id;
+    private String approval_prompt;
+    private String client_secret;
+    private String grant_type="authorization_code";
+    
+    //response
+    private String code;
+    
+    public String getScope(){ return scope; }
+    public String getState(){ return state; }
+    public String getRedirectURI(){ return redirect_uri; }
+    public String getResponseType(){ return response_type; }
+    public String getClientId(){ return client_id; }
+    public String getApprovalPrompt(){ return approval_prompt; }
+    public String getClientSecret(){ return client_secret; }
+    public String getGrantType(){ return grant_type; }
+    public String getCode(){ return code; }
 }
