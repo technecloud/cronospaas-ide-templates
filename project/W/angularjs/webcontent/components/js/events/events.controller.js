cbc07bea9f8908cc491c26797cfc500aba4d8f92
Fábio Costa <fcostasilva@gmail.com>
2015-06-25 16:14:55 -0300

    Corrige mapeamento de arquivo de evento

--- project/W/angularjs/webcontent/components/js/events/events.controller.js
+++ project/W/angularjs/webcontent/components/js/events/events.controller.js
@@ -4,17 +4,27 @@ app.controller('InspectorEventsController', ['PropertyManager','$scope', functio
   $scope.option = "";
   var eventName = "";
   
-  var callback = function(data) {
-  };
+
   
   PropertyManager.on('accept',function(currentObject) {
     var id = currentObject.closest(".component-holder").attr("id");
+
+
+    var callback = function(data) {
+      
+      var script = document.createElement( 'script' );
+      script.type = 'text/javascript';
+
+      script.src = data.filePath;
+      currentObject.closest('.view-content').append( script );
+      
+     currentObject.attr(eventName, data.filePath);
+     
+     
+      PropertyManager.updateCode();
+    };
+
     CronosHandler.processEvents(callback, $scope.side, $scope.option, eventName, id);
-    
-    var script = document.createElement( 'script' );
-    script.type = 'text/javascript';
-    script.src = 'views/events/Pessoa_server.js';
-    currentObject.closest('.view-content').append( script );
     
     return id.split("-").join("_") + "_" + eventName + "()";
   });
