9dee4d50f6741144a0d2794f785b89d6dddbdfec
Fernando <nanndoj@gmail.com>
2015-07-01 13:47:03 -0300

    Criação do componente tabela
    Criação do componente tabela

--- project/W/angularjs/webcontent/components/js/form.js
+++ project/W/angularjs/webcontent/components/js/form.js
@@ -2,18 +2,10 @@
     $app.directive('crnDatasource',['DatasetManager', function(DatasetManager) {
       return {
         restrict: 'A',
-        controller: 'CrnFormController',
         link: function(scope, element, attrs) {
             scope.data = DatasetManager.datasets;
             scope.datasource = scope.data[attrs.crnDatasource];
         }
       };
-    }])
-
-    .controller("CrnFormController",['DatasetManager', '$scope', function(DatasetManager, $scope) {
-        $scope.funcione = function() {
-          $scope.datasource.startInserting();
-        }
-        
     }]);
 }(app));
