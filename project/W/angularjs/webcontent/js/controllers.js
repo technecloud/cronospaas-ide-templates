511d5a8fde7b3bf2f00944292793f4497553e73c
Arthemus C. Moreira <arthemus.moreira@gmail.com>
2015-08-18 16:08:08 -0300

    Controle de sessão
    Alterações para guardar as informações de sessão logo no processo de
    login do sistema.

--- project/W/angularjs/webcontent/js/controllers.js
+++ project/W/angularjs/webcontent/js/controllers.js
@@ -1,71 +1,62 @@
-(function($app) {
-    angular.module('custom.controllers', []);  
+(function ($app) {
+    angular.module('custom.controllers', []);
 
-      app.controller('LoginController', ['$scope', '$http', '$location', '$rootScope', '$window',  function($scope, $http, $location, $rootScope, $window){
+    app.controller('LoginController', ['$scope', '$http', '$location', '$rootScope', '$window', '$state', function ($scope, $http, $location, $rootScope, $window, $state) {
 
         $scope.message = {};
-        
+
         $scope.login = function () {
+
             $scope.message.error = undefined;
 
             var user = { username: username.value, password: password.value };
 
             $http({
-              method  : 'POST',
-              url     : '/auth',
-              data    : $.param(user),  
-              headers : { 'Content-Type': 'application/x-www-form-urlencoded' } 
+                method: 'POST',
+                url: '/auth',
+                data: $.param(user),
+                headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
             }).success(handleSuccess).error(handleError);
+        }
 
-        }
-        
-        $scope.goto = function(path){
-          $window.location.href=path;
-        }
-        
         function handleSuccess(data, status, headers, config) {
-          $location.path('/home');
+
+            function handleSession(response) {
+                $rootScope.session = response.data;
+                $state.go('home');
+            }
+
+            $http({
+                method: 'GET',
+                url: '/session',
+            }).then(handleSession)
         }
 
         function handleError(data, status, headers, config) {
-          var error = status == 401 ? "Username or passoword invalid!" : data ; 
-          $scope.message.error = error;
+            var error = status == 401 ? "Username or passoword invalid!" : data;
+            $scope.message.error = error;
         }
-    }]);  
-    
-    
-    app.controller('HomeController', ['$scope', '$http', '$location','$rootScope','$state', function($scope, $http, $location, $rootScope,$state){
-      $rootScope.session = {};
+    }]);
 
-      $rootScope.logout = function logout() {
+    app.controller('HomeController', ['$scope', '$http', '$location', '$rootScope', '$state', function ($scope, $http, $location, $rootScope, $state) {
 
-          $http({
-            method  : 'GET',
-            url     : '/logout',
-          }).then(handleSuccess, handleError('User or password invalid!'))
-          
-          $rootScope.session = {};
-      }
-      
+        $rootScope.logout = function logout() {
+
+            $http({
+                method: 'GET',
+                url: '/logout',
+            }).then(handleSuccess, handleError('User or password invalid!'))
+
+            $rootScope.session = {};
+        }
+
         function handleSuccess(data) {
-          $location.path('/login');
+            $location.path('/login');
         }
 
 
         function handleError(error) {
-          $rootScope.session.error = error;
+            $rootScope.session.error = error;
         }
-
-        function handleSession(response){
-          $rootScope.session = response.data;
-        }
-
-        $http({
-          method  : 'GET',
-          url     : '/session',
-        }).then(handleSession)
-
     }]);
-    
-    // Your code
-}(app));
+} (app));
