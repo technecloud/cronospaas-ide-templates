ba8a42c715687f36e885f69bf2d9a7f8768fa822
nanndoj <nanndoj@gmail.com>
2015-12-07 18:50:20 -0300

    Correção do bug #PAASMANUT-383

--- project/W/angularjs/src/security/permission/AuthenticationServlet.java
+++ project/W/angularjs/src/security/permission/AuthenticationServlet.java
@@ -1,6 +1,5 @@
 package security.permission;
 
-import java.io.IOException;
 import java.security.SecureRandom;
 import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
@@ -42,241 +41,260 @@ import util.Hash;
 @WebServlet(value = { "/auth", "/logout", "/changePassword" }, name = "auth-servlet")
 public class AuthenticationServlet extends HttpServlet {
 
-	private static final long serialVersionUID = 1L;
+  private static final long serialVersionUID = 1L;
 
-	private static final Logger logger = Logger.getLogger(AuthenticationServlet.class.getName());
+  private static final Logger logger = Logger.getLogger(AuthenticationServlet.class.getName());
 
-	private static final String USERNAME = "admin";
-	private static final String PASSWORD = "admin";
+  private static final String USERNAME = "admin";
+  private static final String PASSWORD = "admin";
 
-	public void init() throws ServletException {
-		//init
-	}
+  @Override
+  public void init() throws ServletException {
+    // init
+  }
 
-	public void destroy() {
-		//destroy
-	}
+  @Override
+  public void destroy() {
+    // destroy
+  }
 
-	protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
-		String uri = req.getRequestURI().substring(req.getContextPath().length());
+  @Override
+  protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    String uri = req.getRequestURI().substring(req.getContextPath().length());
 
-		if ("/auth".equals(uri)) {
-			try {
-				String username = req.getParameter("username");
-				String password = req.getParameter("password");
+    if("/auth".equals(uri)) {
+      try {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
 
-				boolean login = this.login(username, password);
+        boolean login = this.login(username, password);
 
-				if (login) {
-					req.getSession().setAttribute("username", username);
+        if(login) {
+          req.getSession().setAttribute("username", username);
 
-					UserRoleDAO dao = new UserRoleDAO(SessionManager.getInstance().getEntityManager());
+          UserRoleDAO dao = new UserRoleDAO(SessionManager.getInstance().getEntityManager());
 
-					List<UserRole> userRoles = dao.findByLogin(username, Integer.MAX_VALUE, 0);
+          List<UserRole> userRoles = dao.findByLogin(username, Integer.MAX_VALUE, 0);
 
-					String rolesID = AuthorizationFilter.EVERYONE_ID;
+          String rolesID = AuthorizationFilter.EVERYONE_ID;
 
-					for (UserRole userRole : userRoles)
-						rolesID += "," + userRole.getRole().getId();
+          for(UserRole userRole : userRoles) {
+            rolesID += "," + userRole.getRole().getId();
+          }
 
-					req.getSession().setAttribute("roles", rolesID);
+          req.getSession().setAttribute("roles", rolesID);
 
-					User user = this.getUserByName(username);
+          User user = this.getUserByName(username);
 
-					if (user != null) {
-						Gson gson = new Gson();
-						JsonElement json = gson.toJsonTree(user);
-						json.getAsJsonObject().addProperty("roles", rolesID);
-						json.getAsJsonObject().addProperty("root", rolesID.contains(AuthorizationFilter.ADMIN_ID));
-						json.getAsJsonObject().remove("password");
-						resp.setHeader("Content-Type", "application/json");
-						resp.getOutputStream().print(json.toString());
-					} else {
-						resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
-					}
-				} else {
-					resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-				}
-			} catch (Exception e) {
-				throw new CustomWebApplicationException(e);
-			}
-		} else if ("/changePassword".equals(uri)) {
-			changePassword(req, resp);
-		} else {
-			resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-		}
-	}
+          if(user != null) {
+            req.getSession().setAttribute("user", user);
+            Gson gson = new Gson();
+            JsonElement json = gson.toJsonTree(user);
+            json.getAsJsonObject().addProperty("roles", rolesID);
+            json.getAsJsonObject().addProperty("root", rolesID.contains(AuthorizationFilter.ADMIN_ID));
+            json.getAsJsonObject().remove("password");
+            resp.setHeader("Content-Type", "application/json");
+            resp.getOutputStream().print(json.toString());
+          }
+          else {
+            resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+          }
+        }
+        else {
+          resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+        }
+      }
+      catch(Exception e) {
+        throw new CustomWebApplicationException(e);
+      }
+    }
+    else if("/changePassword".equals(uri)) {
+      this.changePassword(req, resp);
+    }
+    else {
+      resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+    }
+  }
 
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
-		try {
-			String uri = req.getRequestURI().substring(req.getContextPath().length());
-			if ("/logout".equals(uri)) {
-				logout(req, resp);
-			} else {
-				resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-			}
-		} catch (Exception e) {
-			throw new CustomWebApplicationException(e);
-		}
-	}
+  @Override
+  protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
+    try {
+      String uri = req.getRequestURI().substring(req.getContextPath().length());
+      if("/logout".equals(uri)) {
+        this.logout(req, resp);
+      }
+      else {
+        resp.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+      }
+    }
+    catch(Exception e) {
+      throw new CustomWebApplicationException(e);
+    }
+  }
 
-	private boolean login(String username, String password) {
-		logger.log(Level.INFO, "login");
-		// return authenticateLocal(username, password);
-		return authenticateDataBase(username, password);
-		// return authenticateOAuth2(username, password);
-	}
+  private boolean login(String username, String password) {
+    logger.log(Level.INFO, "login");
+    // return authenticateLocal(username, password);
+    return this.authenticateDataBase(username, password);
+    // return authenticateOAuth2(username, password);
+  }
 
-	/**
-	 * Validação a operação de login pelas informações locais.
-	 * Método mais simples e menos seguro.
-	 *
-	 * @param username
-	 * @param password
-	 * @return
-	 */
-	private boolean authenticateLocal(String username, String password) {
-		boolean result = USERNAME.equals(username) && PASSWORD.equals(password);
-		if (result)
-			AuthenticationServlet.createUserIfNotExists(username, password, null);
-		return result;
-	}
+  /**
+   * Validação a operação de login pelas informações locais.
+   * Método mais simples e menos seguro.
+   *
+   * @param username
+   * @param password
+   * @return
+   */
+  private boolean authenticateLocal(String username, String password) {
+    boolean result = USERNAME.equals(username) && PASSWORD.equals(password);
+    if(result) {
+      AuthenticationServlet.createUserIfNotExists(username, password, null);
+    }
+    return result;
+  }
 
-	/**
-	 * Busca o usuário pelo servidor de autenticação da Techne utilizando o padrão OAuth2.
-	 *
-	 * @param username
-	 * @param password
-	 * @return
-	 */
-	private boolean authenticateOAuth2(String username, String password) {
-		OAuth2Client client = new OAuth2Client(OAuth2Settings.TOKEN_URI, OAuth2Settings.REVOKE_URI,
-				OAuth2Settings.CLIENT_ID, OAuth2Settings.CLIENT_SECRET);
-		String token = null;
-		try {
-			token = client.authenticate(username, password);
-			createUserIfNotExists(username, username, null);
-			logger.log(Level.INFO, token);
-		} catch (Exception e) {
-			e.printStackTrace();
-			logger.log(Level.SEVERE, e.getMessage());
-		}
-		return (token != null);
-	}
+  /**
+   * Busca o usuário pelo servidor de autenticação da Techne utilizando o padrão OAuth2.
+   *
+   * @param username
+   * @param password
+   * @return
+   */
+  private boolean authenticateOAuth2(String username, String password) {
+    OAuth2Client client = new OAuth2Client(OAuth2Settings.TOKEN_URI, OAuth2Settings.REVOKE_URI,
+            OAuth2Settings.CLIENT_ID, OAuth2Settings.CLIENT_SECRET);
+    String token = null;
+    try {
+      token = client.authenticate(username, password);
+      createUserIfNotExists(username, username, null);
+      logger.log(Level.INFO, token);
+    }
+    catch(Exception e) {
+      e.printStackTrace();
+      logger.log(Level.SEVERE, e.getMessage());
+    }
+    return (token != null);
+  }
 
-	public static void createUserIfNotExists(String name, String username, String pictureURL) {
-		SessionManager session = SessionManager.getInstance();
-		UserDAO userDao = new UserDAO(session.getEntityManager());
-		List<User> users = userDao.findByAttribute("login", username);
-		if (users.isEmpty()) {
-			session.begin();
-			logger.log(Level.INFO, "Creating user: " + username);
-			User userEntity = new User();
-			userEntity.setLogin(username);
-			userEntity.setName(name);
-			userEntity.setPicture(pictureURL);
-			userDao.save(userEntity);
-			session.commit();
-		}
-	}
+  public static void createUserIfNotExists(String name, String username, String pictureURL) {
+    SessionManager session = SessionManager.getInstance();
+    UserDAO userDao = new UserDAO(session.getEntityManager());
+    List<User> users = userDao.findByAttribute("login", username);
+    if(users.isEmpty()) {
+      session.begin();
+      logger.log(Level.INFO, "Creating user: " + username);
+      User userEntity = new User();
+      userEntity.setLogin(username);
+      userEntity.setName(name);
+      userEntity.setPicture(pictureURL);
+      userDao.save(userEntity);
+      session.commit();
+    }
+  }
 
-	/**
-	 * Busca o usuário no banco de dados da aplicação.
-	 *
-	 * @param username
-	 * @param password
-	 * @return
-	 */
-	private boolean authenticateDataBase(String username, String password) {
-		UserBusiness business = new UserBusiness(SessionManager.getInstance());
-		List<User> user = business.findByLogin(username, 100, 0);
-		return user.size() > 0 && Hash.md5(password).equals(user.get(0).getEncodedPassword());
-	}
+  /**
+   * Busca o usuário no banco de dados da aplicação.
+   *
+   * @param username
+   * @param password
+   * @return
+   */
+  private boolean authenticateDataBase(String username, String password) {
+    UserBusiness business = new UserBusiness(SessionManager.getInstance());
+    return business.checkUser(username, password);
+  }
 
-	private ClientConfig configureClientWithSSL() {
-		TrustManager[] certs = new TrustManager[] { new X509TrustManager() {
+  private ClientConfig configureClientWithSSL() {
+    TrustManager[] certs = new TrustManager[] { new X509TrustManager() {
 
-			@Override
-			public X509Certificate[] getAcceptedIssuers() {
-				return null;
-			}
+      @Override
+      public X509Certificate[] getAcceptedIssuers() {
+        return null;
+      }
 
-			@Override
-			public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-			}
+      @Override
+      public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+      }
 
-			@Override
-			public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-			}
-		} };
+      @Override
+      public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
+      }
+    } };
 
-		SSLContext ctx = null;
-		try {
-			ctx = SSLContext.getInstance("TLS");
-			ctx.init(null, certs, new SecureRandom());
-		} catch (java.security.GeneralSecurityException ex) {
-			// NoCommand
-		}
+    SSLContext ctx = null;
+    try {
+      ctx = SSLContext.getInstance("TLS");
+      ctx.init(null, certs, new SecureRandom());
+    }
+    catch(java.security.GeneralSecurityException ex) {
+      // NoCommand
+    }
 
-		HttpsURLConnection.setDefaultSSLSocketFactory(ctx.getSocketFactory());
-		ClientConfig config = new DefaultClientConfig();
-		try {
-			config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES,
-					new HTTPSProperties((hostname, session) -> true, ctx));
-		} catch (Exception e) {
-			// NoCommand
-		}
+    HttpsURLConnection.setDefaultSSLSocketFactory(ctx.getSocketFactory());
+    ClientConfig config = new DefaultClientConfig();
+    try {
+      config.getProperties().put(HTTPSProperties.PROPERTY_HTTPS_PROPERTIES,
+              new HTTPSProperties((hostname, session) -> true, ctx));
+    }
+    catch(Exception e) {
+      // NoCommand
+    }
 
-		return config;
-	}
+    return config;
+  }
 
-	private Client createClientWithSSL() {
-		return Client.create(configureClientWithSSL());
-	}
+  private Client createClientWithSSL() {
+    return Client.create(this.configureClientWithSSL());
+  }
 
-	private void logout(HttpServletRequest request, HttpServletResponse response) {
-		Object accessToken = request.getSession().getAttribute("accessToken");
-		request.getSession().invalidate();
-		if (accessToken != null) {
-			try {
-				OAuth2CodeSettings settings = (OAuth2CodeSettings) request.getSession().getAttribute("settings");
-				RevokeServlet.revoke(settings, accessToken.toString());
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
-		}
-	}
+  private void logout(HttpServletRequest request, HttpServletResponse response) {
+    Object accessToken = request.getSession().getAttribute("accessToken");
+    request.getSession().invalidate();
+    if(accessToken != null) {
+      try {
+        OAuth2CodeSettings settings = (OAuth2CodeSettings)request.getSession().getAttribute("settings");
+        RevokeServlet.revoke(settings, accessToken.toString());
+      }
+      catch(Exception e) {
+        e.printStackTrace();
+      }
+    }
+  }
 
-	private void changePassword(HttpServletRequest req, HttpServletResponse resp) {
-		Object username = req.getSession().getAttribute("username");
-		if (username != null) {
-			User user = this.getUserByName(username.toString());
-			if (user != null) {
-				String oldPassword = req.getParameter("oldPassword");
-				String newPassword = req.getParameter("newPassword");
-				String newPasswordConfirmation = req.getParameter("newPasswordConfirmation");
+  private void changePassword(HttpServletRequest req, HttpServletResponse resp) {
+    Object username = req.getSession().getAttribute("username");
+    if(username != null) {
+      User user = this.getUserByName(username.toString());
+      if(user != null) {
+        String oldPassword = req.getParameter("oldPassword");
+        String newPassword = req.getParameter("newPassword");
+        String newPasswordConfirmation = req.getParameter("newPasswordConfirmation");
 
-				if (user.getEncodedPassword().equals(Hash.md5(oldPassword)) && newPassword.equals(newPasswordConfirmation)
-						&& !newPassword.isEmpty()) {
-					user.setPassword(newPassword);
-					SessionManager session = SessionManager.getInstance();
-					UserBusiness business = new UserBusiness(session);
-					session.begin();
-					business.update(user);
-					session.commit();
-				} else {
-					resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
-				}
-			}
-		}
-	}
+        if(user.getPassword().equals(Hash.md5(oldPassword)) && newPassword.equals(newPasswordConfirmation) &&
+                !newPassword.isEmpty()) {
+          user.setPassword(newPassword);
+          SessionManager session = SessionManager.getInstance();
+          UserBusiness business = new UserBusiness(session);
+          session.begin();
+          business.update(user);
+          session.commit();
+        }
+        else {
+          resp.setStatus(HttpServletResponse.SC_FORBIDDEN);
+        }
+      }
+    }
+  }
 
-	private User getUserByName(String username) {
-		UserDAO userDao = new UserDAO(SessionManager.getInstance().getEntityManager());
-		List<User> users = userDao.findByAttribute("login", username.toString());
-		if (!users.isEmpty())
-			return users.get(0);
-		return null;
-	}
+  private User getUserByName(String username) {
+    UserDAO userDao = new UserDAO(SessionManager.getInstance().getEntityManager());
+    List<User> users = userDao.findByAttribute("login", username.toString());
+    if(!users.isEmpty()) {
+      return users.get(0);
+    }
+    return null;
+  }
 
 }
