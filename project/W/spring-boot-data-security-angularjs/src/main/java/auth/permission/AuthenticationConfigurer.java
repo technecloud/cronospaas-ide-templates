fe881368746aa00dd84f2f0c09b7ddac8a24e0cc
Robson Ataíde <robsonataide@gmail.com>
2016-05-18 16:56:07 -0300

    Comentários para SpringSecurity

--- project/W/spring-boot-data-security-angularjs/src/main/java/auth/permission/AuthenticationConfigurer.java
+++ project/W/spring-boot-data-security-angularjs/src/main/java/auth/permission/AuthenticationConfigurer.java
@@ -21,7 +21,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
-
 import security.dao.PermissionDAO;
 import security.dao.RoleDAO;
 import security.dao.UserDAO;
@@ -31,123 +30,151 @@ import security.entity.Role;
 import security.entity.User;
 import security.entity.UserRole;
 
-
+/**
+ * Classe que representa o componente customizado responsável pela autenticação
+ * 
+ * @author Humberto Dias
+ *
+ */
 @Component
 public class AuthenticationConfigurer implements AuthenticationProvider {
-	private static final Logger LOGGER = LoggerFactory
-			.getLogger(AuthenticationConfigurer.class);
-
-	private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-	@Autowired
-	private UserDAO userRepository;
-
-	@Autowired
-	private UserRoleDAO userRoleRepository;
-
-	@Autowired
-	private RoleDAO roleRepository;
-
-	@Autowired
-	private PermissionDAO permissionRepository;
-
-	public final String ROLE_ADMIN_ID = "00000000-0000-0000-0000-000000000000";
-	public final String ROLE_LOGGED_ID = "11111111-1111-1111-1111-111111111111";
-	private static Role ROLE_LOGGED = null;
-	public static final String ROLE_ADMIN_NAME = "Administrators";
-
-	public void createDatabase() {
-
-		LOGGER.info("Creating database");
-
-		User userAdmin = new User();
-		userAdmin.setName("admin").setLogin("admin").setPassword("admin");
-
-		Role roleAdmin = new Role();
-		roleAdmin.setId(ROLE_ADMIN_ID).setName(ROLE_ADMIN_NAME);
-
-		Permission permissionAdmin = new Permission();
-		permissionAdmin.setPath("/views/admin/**").setVerb("ALL")
-				.setRole(roleAdmin).setPriority(1).setEnabled(true);
-		Permission permissionAdminRest = new Permission();
-		permissionAdminRest.setPath("/api/rest/security/**").setVerb("ALL")
-				.setRole(roleAdmin).setPriority(1).setEnabled(true);
-
-		Role roleLogged = new Role();
-		roleLogged.setId(ROLE_LOGGED_ID).setName("Logged");
-
-		Permission permissionLogged = new Permission();
-		permissionLogged.setPath("/views/logged/**").setVerb("ALL")
-				.setRole(roleLogged).setPriority(1).setEnabled(true);
-
-		UserRole userRoleAdmin = new UserRole();
-		userRoleAdmin.setRole(roleAdmin).setUser(userAdmin);
-
-		User userOrdinary = new User();
-		userOrdinary.setName("techne").setLogin("techne").setPassword("techne");
-
-		userRepository.save(userOrdinary);
-		userRepository.save(userAdmin);
-		roleRepository.save(roleAdmin);
-		roleRepository.save(roleLogged);
-		permissionRepository.save(permissionAdmin);
-		permissionRepository.save(permissionLogged);
-		permissionRepository.save(permissionAdminRest);
-		userRoleRepository.save(userRoleAdmin);
-	}
-
-	@Override
-	public Authentication authenticate(Authentication authentication)
-			throws AuthenticationException {
-		String name = authentication.getName();
-		String rawPassword = authentication.getCredentials().toString();
-		List<User> users = userRepository.findByLogin(name, new PageRequest(0,
-				100));
-
-		if (users.isEmpty())
-			throw new UsernameNotFoundException("Usuario nao encontrado!");
-
-		User user = users.get(0);
-		if (passwordEncoder.matches(rawPassword, user.getPassword())) {
-			Set<GrantedAuthority> roles = getAuthorities(user);
-			org.springframework.security.core.userdetails.User userDetails = new org.springframework.security.core.userdetails.User(
-					user.getName(), user.getPassword(), false, false, false,
-					false, roles);
-			UsernamePasswordAuthenticationToken userToken = new UsernamePasswordAuthenticationToken(
-					userDetails, user.getPassword(), roles);
-			userToken.setDetails(userDetails);
-			return userToken;
-		} else {
-			throw new BadCredentialsException("Usuario ou senha incorreta!");
-		}
-	}
-
-	@Override
-	public boolean supports(Class<?> authentication) {
-		return authentication.equals(UsernamePasswordAuthenticationToken.class);
-	}
-
-	private Set<GrantedAuthority> getAuthorities(User user) {
-		Set<GrantedAuthority> authorities = new HashSet<>();
-
-		Pageable pageable = new PageRequest(0, 100);
-		List<UserRole> roles = userRoleRepository.findByLogin(user.getLogin(),
-				pageable);
-		for (UserRole userRole : roles) {
-			GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(
-					userRole.getRole().getName());
-			authorities.add(grantedAuthority);
-		}
-
-		if (ROLE_LOGGED == null)
-			ROLE_LOGGED = roleRepository.findOne(ROLE_LOGGED_ID);
-
-		// Virtual Role Logged
-		authorities.add(new SimpleGrantedAuthority(ROLE_LOGGED.getName()));
-
-		LOGGER.debug("user authorities are " + authorities.toString());
-		return authorities;
-	}
-
-
+  private static final Logger LOGGER = LoggerFactory.getLogger(AuthenticationConfigurer.class);
+  
+  /**
+   * Componente para criptografar senhas
+   */
+  private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+  
+  /**
+   * DAO de User
+   */
+  @Autowired
+  private UserDAO userRepository;
+  
+  /**
+   * DAO de UserRole
+   */
+  @Autowired
+  private UserRoleDAO userRoleRepository;
+  
+  /**
+   * DAO de Role
+   */
+  @Autowired
+  private RoleDAO roleRepository;
+  
+  /**
+   * DAO de Permission
+   */
+  @Autowired
+  private PermissionDAO permissionRepository;
+  
+  /**
+   * Contantes utilizadas para definir valores para administradores e usuários logados,
+   * serão utilizadas no processo de criação da base inicial
+   */
+  public final String ROLE_ADMIN_ID = "00000000-0000-0000-0000-000000000000";
+  public final String ROLE_LOGGED_ID = "11111111-1111-1111-1111-111111111111";
+  private static Role ROLE_LOGGED = null;
+  public static final String ROLE_ADMIN_NAME = "Administrators";
+  
+  /**
+   * Método que criará a base inicial para o esquema de segurança
+   */
+  public void createDatabase() {
+    
+    LOGGER.info("Creating database");
+    
+    User userAdmin = new User();
+    userAdmin.setName("admin").setLogin("admin").setPassword("admin");
+    
+    Role roleAdmin = new Role();
+    roleAdmin.setId(ROLE_ADMIN_ID).setName(ROLE_ADMIN_NAME);
+    
+    Permission permissionAdmin = new Permission();
+    permissionAdmin.setPath("/views/admin/**").setVerb("ALL").setRole(roleAdmin).setPriority(1).setEnabled(true);
+    Permission permissionAdminRest = new Permission();
+    permissionAdminRest.setPath("/api/rest/security/**").setVerb("ALL").setRole(roleAdmin).setPriority(1)
+            .setEnabled(true);
+    
+    Role roleLogged = new Role();
+    roleLogged.setId(ROLE_LOGGED_ID).setName("Logged");
+    
+    Permission permissionLogged = new Permission();
+    permissionLogged.setPath("/views/logged/**").setVerb("ALL").setRole(roleLogged).setPriority(1).setEnabled(true);
+    
+    UserRole userRoleAdmin = new UserRole();
+    userRoleAdmin.setRole(roleAdmin).setUser(userAdmin);
+    
+    User userOrdinary = new User();
+    userOrdinary.setName("techne").setLogin("techne").setPassword("techne");
+    
+    userRepository.save(userOrdinary);
+    userRepository.save(userAdmin);
+    roleRepository.save(roleAdmin);
+    roleRepository.save(roleLogged);
+    permissionRepository.save(permissionAdmin);
+    permissionRepository.save(permissionLogged);
+    permissionRepository.save(permissionAdminRest);
+    userRoleRepository.save(userRoleAdmin);
+  }
+  
+  /**
+   * Método customizado para autenticação de usuário
+   */
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    String name = authentication.getName();
+    String rawPassword = authentication.getCredentials().toString();
+    List<User> users = userRepository.findByLogin(name, new PageRequest(0, 100));
+    
+    if(users.isEmpty())
+      throw new UsernameNotFoundException("Usuario nao encontrado!");
+    
+    User user = users.get(0);
+    if(passwordEncoder.matches(rawPassword, user.getPassword())) {
+      Set<GrantedAuthority> roles = getAuthorities(user);
+      org.springframework.security.core.userdetails.User userDetails = new org.springframework.security.core.userdetails.User(
+              user.getName(), user.getPassword(), false, false, false, false, roles);
+      UsernamePasswordAuthenticationToken userToken = new UsernamePasswordAuthenticationToken(userDetails,
+              user.getPassword(), roles);
+      userToken.setDetails(userDetails);
+      return userToken;
+    }
+    else {
+      throw new BadCredentialsException("Usuario ou senha incorreta!");
+    }
+  }
+  
+  /**
+   * Valida se o objeto Authentication é suportado pelo provider
+   */
+  @Override
+  public boolean supports(Class<?> authentication) {
+    return authentication.equals(UsernamePasswordAuthenticationToken.class);
+  }
+  
+  /**
+   * Recupera as autoridades concedidas para um usuário
+   */
+  private Set<GrantedAuthority> getAuthorities(User user) {
+    Set<GrantedAuthority> authorities = new HashSet<>();
+    
+    Pageable pageable = new PageRequest(0, 100);
+    List<UserRole> roles = userRoleRepository.findByLogin(user.getLogin(), pageable);
+    for(UserRole userRole : roles) {
+      GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(userRole.getRole().getName());
+      authorities.add(grantedAuthority);
+    }
+    
+    if(ROLE_LOGGED == null)
+      ROLE_LOGGED = roleRepository.findOne(ROLE_LOGGED_ID);
+    
+    // Virtual Role Logged
+    authorities.add(new SimpleGrantedAuthority(ROLE_LOGGED.getName()));
+    
+    LOGGER.debug("user authorities are " + authorities.toString());
+    return authorities;
+  }
+  
 }
--- project/W/spring-boot-data-security-angularjs/src/main/java/auth/permission/ChangePassword.java
+++ project/W/spring-boot-data-security-angularjs/src/main/java/auth/permission/ChangePassword.java
@@ -15,42 +15,47 @@ import java.util.*;
 import security.entity.*;
 import security.business.*;
 
+/**
+ * Controller responsável por gerir a troca de
+ * senha do usuário através de serviço REST
+ * 
+ * @author Humberto Dias
+ *
+ */
 @RestController
 @RequestMapping(value = "/changePassword")
 @PreAuthorize("hasRole('Logged')")
 public class ChangePassword {
-
-	@Autowired
-	@Qualifier("UserBusiness")
-	private UserBusiness userBusiness;
-
-	private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-
-
-	@RequestMapping(method = RequestMethod.POST)
-	@ResponseStatus(HttpStatus.OK)
-	public User post(final String oldPassword, final String newPassword,
-			final String newPasswordConfirmation) throws Exception {
-
-		if (!newPassword.equals(newPasswordConfirmation))
-			throw new RuntimeException("Senha de confirmação diferente");
-
-		org.springframework.security.core.userdetails.User userDetails = (org.springframework.security.core.userdetails.User) SecurityContextHolder
-				.getContext().getAuthentication().getPrincipal();
-
-		List<User> users = userBusiness.getRepository().findByLogin(
-				userDetails.getUsername(), new PageRequest(0, 100));
-		if (users.size() > 0) {
-			User user = users.get(0);
-
-  		if (!passwordEncoder.matches(oldPassword, user.getPassword()))
-    		throw new RuntimeException("Senha anterior não confere!");
-
-			user.setPassword(newPassword);
-			userBusiness.getRepository().saveAndFlush(user);
-			return user;
-		}
-
-		throw new RuntimeException("Usuario não encontrado!");
-	}
+  
+  @Autowired
+  @Qualifier("UserBusiness")
+  private UserBusiness userBusiness;
+  
+  private final PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+  
+  @RequestMapping(method = RequestMethod.POST)
+  @ResponseStatus(HttpStatus.OK)
+  public User post(final String oldPassword, final String newPassword, final String newPasswordConfirmation)
+          throws Exception {
+    
+    if(!newPassword.equals(newPasswordConfirmation))
+      throw new RuntimeException("Senha de confirmação diferente");
+    
+    org.springframework.security.core.userdetails.User userDetails = (org.springframework.security.core.userdetails.User)SecurityContextHolder
+            .getContext().getAuthentication().getPrincipal();
+    
+    List<User> users = userBusiness.getRepository().findByLogin(userDetails.getUsername(), new PageRequest(0, 100));
+    if(users.size() > 0) {
+      User user = users.get(0);
+      
+      if(!passwordEncoder.matches(oldPassword, user.getPassword()))
+        throw new RuntimeException("Senha anterior não confere!");
+      
+      user.setPassword(newPassword);
+      userBusiness.getRepository().saveAndFlush(user);
+      return user;
+    }
+    
+    throw new RuntimeException("Usuario não encontrado!");
+  }
 }
