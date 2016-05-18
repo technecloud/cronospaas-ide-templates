9cac6008ca0bc2a1fb1e3b51597a2ffcfcdd0778
Humberto Dias <hldtux@gmail.com>
2016-05-18 14:33:48 -0300

    Garantindo unicidade em UserRole(user,role).

--- project/W/spring-boot-data-security-angularjs/src/main/java/auth/permission/AuthorizationConfigurer.java
+++ project/W/spring-boot-data-security-angularjs/src/main/java/auth/permission/AuthorizationConfigurer.java
@@ -73,22 +73,21 @@ public class AuthorizationConfigurer extends WebSecurityConfigurerAdapter {
 
 		// public 
 		String [] publics = {"/index.html", "/views/login.view.html", "/public/**", "/plugins/**", "/components/**", "/js/**", "/css/**", "/img/**", "/i18n/**", "/views/error/**"};
-		http.authorizeRequests()
-        .antMatchers(publics).permitAll()
-        .anyRequest().authenticated();
+		http.authorizeRequests().antMatchers(publics).permitAll();
 		
 		List<Permission> permissions = permissionRepository.findAll();
 		for (Permission p : permissions) {
-			http.authorizeRequests().antMatchers(method(p.getVerb()), p.getPath()).hasAuthority(p.getRole().getName())
-					.anyRequest().authenticated();
+			http.authorizeRequests().antMatchers(method(p.getVerb()), p.getPath()).hasAuthority(p.getRole().getName());
 		}
 		
-//		http.authorizeRequests().anyRequest().anonymous();
+		// denyAll
+		http.authorizeRequests().antMatchers("/**").denyAll();
 
 		// login/logout
 		http
 		  .formLogin()
     		.loginProcessingUrl("/auth")
+ 		    .loginPage("/")
     		.successHandler(successHandler())
     		.failureHandler(failureHandler())
   	.and()
