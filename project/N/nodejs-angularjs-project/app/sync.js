42b870d6c3469fa60d4f95a7101fa316704cd8b6
Tiago Romano <tiago.romano@techne.com.br>
2016-12-15 16:33:12 -0300

    Ajuste template do NodeJs (adicionado modulos e criação de loaders)

---
+++ project/N/nodejs-angularjs-project/app/sync.js
@@ -0,0 +1,5 @@
+var modelsToSync = ['./main/models'];
+modelsToSync.forEach(function(model) { 
+  var m = require(model);
+  m.sequelize.sync();
+});
---
+++ project/N/nodejs-angularjs-project/server.js
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+var serverPort = process.env.VCAP_APP_PORT || 8888;
+var fs = require('fs');
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+app.use(bodyParser());
+app.use(express.static('./public'));
+
+require('./app/sync');
+require('./app/loader_rest')(app);
+
+app.get('/run/:id', function(req, res){
+  var id = req.params('id');
+  res.writeHead(200, {"Content-Type": "text/html"});  
+  res.end("Action received param: " + id);
+});
+
+var server = app.listen(serverPort, function () {
+  console.log('Started');
+});
