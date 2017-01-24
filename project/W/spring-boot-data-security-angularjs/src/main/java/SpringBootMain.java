55aa7fd21213988d2fc2842a6e47d0c759567e38
Tiago Romano <tiago.romano@techne.com.br>
2017-01-24 16:59:44 -0300

    Modificado o SpringBootMain.java.ftl para adicionar o "api.rest.webservices" no basePackages

--- project/W/spring-boot-data-security-angularjs/src/main/java/SpringBootMain.java
+++ project/W/spring-boot-data-security-angularjs/src/main/java/SpringBootMain.java
@@ -10,7 +10,7 @@ import org.springframework.context.annotation.*;
  *  - Executar qualquer bean do tipo CommandLineRunner;
  */
 @ComponentScan(basePackages = {
-  "auth.permission", "api.rest.events", "app", "reports"
+  "auth.permission", "api.rest.events", "api.rest.webservices", "app", "reports"
 })
 @SpringBootApplication
 public class SpringBootMain extends SpringBootServletInitializer {
