fe2ec02c8ef721b299962771ec2abaf80b19c7fa
tarsomatos <tarsomatos@gmail.com>
2017-01-24 08:23:03 -0300

    Correção de Emergencia. Faltando rotina responsavel por remover o splash screen e adicionado controler do listCanSwipe

--- project/M/mobile-web-spring-boot-phonegap/src/main/webapp-sem-autenticacao/webapp/js/app.js
+++ project/M/mobile-web-spring-boot-phonegap/src/main/webapp-sem-autenticacao/webapp/js/app.js
@@ -24,6 +24,29 @@ var app = (function() {
       },
       'preferredLocale': 'pt_BR'
     })
+    .run(function($ionicPlatform) {
+        $ionicPlatform.ready(function() {
+            // Remove splash screen
+            setTimeout(function() {
+              if (navigator.splashscreen) {
+                navigator.splashscreen.hide();
+              }
+            }, 100);
+            // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+            // for form inputs)
+            if (window.cordova &&
+                window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard
+                    .hideKeyboardAccessoryBar(true);
+                cordova.plugins.Keyboard.disableScroll(true);
+
+            }
+            if (window.StatusBar) {
+                // org.apache.cordova.statusbar required
+                StatusBar.styleDefault();
+            }
+        });
+    })
       .config([
       '$httpProvider',
       function($httpProvider) {
