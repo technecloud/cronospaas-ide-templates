492caec70710a33dc57876e46be6b2c3f50a9f2b
Tiago Romano <tiago.romano@techne.com.br>
2017-03-14 17:09:16 -0300

    Ajustes no evento criado para o botão - MOBILE

--- project/M/mobile-web-spring-boot-phonegap/src/main/webapp-sem-autenticacao/webapp/js/controllers.js
+++ project/M/mobile-web-spring-boot-phonegap/src/main/webapp-sem-autenticacao/webapp/js/controllers.js
@@ -2,7 +2,8 @@
     angular.module('custom.controllers', []);
     
     app.controller('HomeController', ['$scope', '$http', '$rootScope', '$state', '$translate', 'Notification', function ($scope, $http, $rootScope, $state, $translate, Notification) {
-        
+        for(var x in app.userEvents)
+          $scope[x]= app.userEvents[x].bind($scope);
         $scope.message = {};
         $scope.shouldShowDelete = false;
         $scope.shouldShowReorder = false;
