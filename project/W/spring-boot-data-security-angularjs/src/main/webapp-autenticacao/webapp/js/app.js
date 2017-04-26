b8b96fad945a44395b8471cc13403d4801bfc320
Tiago Romano <tiago.romano@techne.com.br>
2017-04-26 17:20:51 -0300

    Ajustes no tempalte do springboot para as funções do cronapi

--- project/W/spring-boot-data-security-angularjs/src/main/webapp-autenticacao/webapp/js/app.js
+++ project/W/spring-boot-data-security-angularjs/src/main/webapp-autenticacao/webapp/js/app.js
@@ -35,7 +35,10 @@ var app = (function() {
             var service = {
               'request': function(config) {
                 var _u = JSON.parse(sessionStorage.getItem('_u'));
-                if (_u && _u.token) config.headers['X-AUTH-TOKEN'] = _u.token;
+                if (_u && _u.token) {
+                  config.headers['X-AUTH-TOKEN'] = _u.token;
+                  window.uToken = _u.token;
+                }
                 return config;
               }
             };
