1260da3d4fb56c7736c41bffc651dbaedc9bbdf5
Arthemus Cicotti Moreira <arthemus.moreira@gmail.com>
2017-07-21 15:33:59 -0300

    Correção ao verificar a existencia do usuário.

--- project/W/cronapp-rad-project/src/main/java/tenant/TenantComponentImpl.java
+++ project/W/cronapp-rad-project/src/main/java/tenant/TenantComponentImpl.java
@@ -15,28 +15,30 @@ import cronapp.framework.tenant.TenantService;
 
 @Component
 public class TenantComponentImpl implements TenantComponent {
-  
-  private static final Logger log = LoggerFactory.getLogger(TenantComponentImpl.class);
-  
-  private final TenantService tenantService;
-  
-  private final UserBusiness userBusiness;
-  
-  @Autowired
-  public TenantComponentImpl(TenantService tenantService, UserBusiness userBusiness) {
-    this.tenantService = tenantService;
-    this.userBusiness = userBusiness;
-  }
-  
-  @Override
-  public void authenticationTenant(String username) {
-    log.info("Tenant [User:Company:id] executed");
-    Page<User> page = userBusiness.findByLogin(username, null);
-    if(page != null && page.getSize() > 0) {
-      List<User> content = page.getContent();
-      User user = content.get(0);
-      this.tenantService.setId("tenant", user.getCompany().getId());
-    }
-  }
-  
+
+	private static final Logger log = LoggerFactory.getLogger(TenantComponentImpl.class);
+
+	private final TenantService tenantService;
+
+	private final UserBusiness userBusiness;
+
+	@Autowired
+	public TenantComponentImpl(TenantService tenantService, UserBusiness userBusiness) {
+		this.tenantService = tenantService;
+		this.userBusiness = userBusiness;
+	}
+
+	@Override
+	public void authenticationTenant(String username) {
+		log.info("Tenant [User:Company:id] executed");
+		Page<User> page = userBusiness.findByLogin(username, null);
+		if (page != null) {
+			List<User> content = page.getContent();
+			if (!content.isEmpty()) {
+				User user = content.get(0);
+				this.tenantService.setId("tenant", user.getCompany().getId());
+			}
+		}
+	}
+
 }
