eaeadd7c30efb00b4f3dfd05d575f56ffda46df6
Thiago Costa <thiagoalvescosta@gmail.com>
2017-08-03 19:41:06 -0300

    Movendo tenant para framework

--- project/W/cronapp-rad-project/src/main/java/app/test/DepartmentTest.java
+++
@@ -1,9 +0,0 @@
-package app.test;
-
-/**
- * Classe utilitária de teste Department
- * @generated
- **/
-public class DepartmentTest {
-	
-}
--- project/W/cronapp-rad-project/src/main/java/app/test/UserRoleTest.java
+++
@@ -1,9 +0,0 @@
-package app.test;
-
-/**
- * Classe utilitária de teste UserRole
- * @generated
- **/
-public class UserRoleTest {
-	
-}
