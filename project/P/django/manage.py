1bcdde1f2ab906aab2a2502df5049faf6b06c222
Henrique Chang <henrique.queiroz@cronapp.io>
2018-10-16 09:44:00 -0300

    Ajustes na configuração do template django

--- project/P/django/manage.py
+++ project/P/django/manage.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python
 import os
 import sys
+from os import environ
+from django.core.management.commands.runserver import Command as runserver
+runserver.default_port = environ.get('VCAP_APP_PORT')
+runserver.default_addr = '0.0.0.0'
 
 if __name__ == '__main__':
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cronapp.settings')
