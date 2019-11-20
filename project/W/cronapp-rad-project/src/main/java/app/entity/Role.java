3d4a9cbab923a322530d68a1efa3d113214f6b07
Michel Moreira <michel@michelmoreira.eti.br>
2019-11-20 08:36:51 -0300

    CRONAPP-165 Permissões Dinâmicas

--- project/W/cronapp-rad-project/src/main/java/app/entity/Role.java
+++ project/W/cronapp-rad-project/src/main/java/app/entity/Role.java
@@ -7,188 +7,190 @@ import javax.xml.bind.annotation.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import cronapi.rest.security.CronappSecurity;
+import org.eclipse.persistence.annotations.Convert;
+import org.eclipse.persistence.annotations.Converter;
+import cronapi.database.VersionConverter;
 
 
 /**
- * Classe que representa a tabela role
- * @generated
- */
+* Classe que representa a tabela role
+* @generated
+*/
 @Entity
 @Table(name = "\"role\"")
 @XmlRootElement
 @CronappSecurity(post = "Administrators", get = "Administrators", delete = "Administrators", put = "Administrators")
 @JsonFilter("app.entity.Role")
+@Converter(
+  name="version",
+  converterClass=VersionConverter.class
+)
 public class Role implements Serializable {
 
-  /**
-   * UID da classe, necessário na serialização
-   * @generated
-   */
-  private static final long serialVersionUID = 1L;
+    /**
+    * UID da classe, necessário na serialização
+    * @generated
+    */
+    private static final long serialVersionUID = 1L;
 
-  /**
-   * @generated
-   */
-  @Id
-  @Column(name = "id", nullable = false, length=255, insertable=true, updatable=true)
-  private java.lang.String id;
+    /**
+    * @generated
+    */
+    @Id
+    @Column(name = "id", nullable = false, length=255, insertable=true, updatable=true)
+    private java.lang.String id;
 
-  /**
-  * @generated
-  */
-  @Column(name = "builtin", nullable = false, unique = false, insertable=true, updatable=true)
-  
-  private java.lang.Boolean builtin;
+    /**
+    * @generated
+    */
+    @Column(name = "builtin", nullable = false, unique = false, insertable=true, updatable=true)
+    
+    private java.lang.Boolean builtIn;
 
-  /**
-  * @generated
-  */
-  @Column(name = "membership_enabled", nullable = false, unique = false, insertable=true, updatable=true)
-  
-  private java.lang.Boolean membershipEnabled;
+    /**
+    * @generated
+    */
+    @Column(name = "membership_enabled", nullable = false, unique = false, insertable=true, updatable=true)
+    
+    private java.lang.Boolean membershipEnabled;
 
-  /**
-  * @generated
-  */
-  @Column(name = "name", nullable = false, unique = false, length=255, insertable=true, updatable=true)
-  
-  private java.lang.String name;
+    /**
+    * @generated
+    */
+    @Column(name = "name", nullable = false, unique = false, length=255, insertable=true, updatable=true)
+    
+    private java.lang.String name;
 
-  /**
-  * @generated
-  */
-  @Column(name = "normalized_name", nullable = false, unique = false, length=255, insertable=true, updatable=true)
-  
-  private java.lang.String normalizedName;
+    /**
+    * @generated
+    */
+    @Column(name = "normalized_name", nullable = false, unique = false, length=255, insertable=true, updatable=true)
+    
+    private java.lang.String normalizedName;
 
-  /**
-   * Construtor
-   * @generated
-   */
-  public Role(){
-  }
+    /**
+    * Construtor
+    * @generated
+    */
+    public Role(){
+    }
 
+    /**
+    * Obtém id
+    * return id
+    * @generated
+    */
+    
+    public java.lang.String getId(){
+        return this.id;
+    }
 
-  /**
-   * Obtém id
-   * return id
-   * @generated
-   */
-  
-  public java.lang.String getId(){
-    return this.id;
-  }
+    /**
+    * Define id
+    * @param id id
+    * @generated
+    */
+    public Role setId(java.lang.String id){
+        this.id = id;
+        return this;
+    }
+    /**
+    * Obtém builtIn
+    * return builtIn
+    * @generated
+    */
+    
+    public java.lang.Boolean getBuiltIn(){
+        return this.builtIn;
+    }
 
-  /**
-   * Define id
-   * @param id id
-   * @generated
-   */
-  public Role setId(java.lang.String id){
-    this.id = id;
-    return this;
-  }
+    /**
+    * Define builtIn
+    * @param builtIn builtIn
+    * @generated
+    */
+    public Role setBuiltIn(java.lang.Boolean builtIn){
+        this.builtIn = builtIn;
+        return this;
+    }
+    /**
+    * Obtém membershipEnabled
+    * return membershipEnabled
+    * @generated
+    */
+    
+    public java.lang.Boolean getMembershipEnabled(){
+        return this.membershipEnabled;
+    }
 
-  /**
-   * Obtém builtin
-   * return builtin
-   * @generated
-   */
-  
-  public java.lang.Boolean getBuiltin(){
-    return this.builtin;
-  }
+    /**
+    * Define membershipEnabled
+    * @param membershipEnabled membershipEnabled
+    * @generated
+    */
+    public Role setMembershipEnabled(java.lang.Boolean membershipEnabled){
+        this.membershipEnabled = membershipEnabled;
+        return this;
+    }
+    /**
+    * Obtém name
+    * return name
+    * @generated
+    */
+    
+    public java.lang.String getName(){
+        return this.name;
+    }
 
-  /**
-   * Define builtin
-   * @param builtin builtin
-   * @generated
-   */
-  public Role setBuiltin(java.lang.Boolean builtin){
-    this.builtin = builtin;
-    return this;
-  }
+    /**
+    * Define name
+    * @param name name
+    * @generated
+    */
+    public Role setName(java.lang.String name){
+        this.name = name;
+        return this;
+    }
+    /**
+    * Obtém normalizedName
+    * return normalizedName
+    * @generated
+    */
+    
+    public java.lang.String getNormalizedName(){
+        return this.normalizedName;
+    }
 
-  /**
-   * Obtém membershipEnabled
-   * return membershipEnabled
-   * @generated
-   */
-  
-  public java.lang.Boolean getMembershipEnabled(){
-    return this.membershipEnabled;
-  }
+    /**
+    * Define normalizedName
+    * @param normalizedName normalizedName
+    * @generated
+    */
+    public Role setNormalizedName(java.lang.String normalizedName){
+        this.normalizedName = normalizedName;
+        return this;
+    }
 
-  /**
-   * Define membershipEnabled
-   * @param membershipEnabled membershipEnabled
-   * @generated
-   */
-  public Role setMembershipEnabled(java.lang.Boolean membershipEnabled){
-    this.membershipEnabled = membershipEnabled;
-    return this;
-  }
+    /**
+    * @generated
+    */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Role object = (Role)obj;
+        if (id != null ? !id.equals(object.id) : object.id != null) return false;
+        return true;
+    }
 
-  /**
-   * Obtém name
-   * return name
-   * @generated
-   */
-  
-  public java.lang.String getName(){
-    return this.name;
-  }
-
-  /**
-   * Define name
-   * @param name name
-   * @generated
-   */
-  public Role setName(java.lang.String name){
-    this.name = name;
-    return this;
-  }
-
-  /**
-   * Obtém normalizedName
-   * return normalizedName
-   * @generated
-   */
-  
-  public java.lang.String getNormalizedName(){
-    return this.normalizedName;
-  }
-
-  /**
-   * Define normalizedName
-   * @param normalizedName normalizedName
-   * @generated
-   */
-  public Role setNormalizedName(java.lang.String normalizedName){
-    this.normalizedName = normalizedName;
-    return this;
-  }
-
-  /**
-   * @generated
-   */
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
-    Role object = (Role)obj;
-    if (id != null ? !id.equals(object.id) : object.id != null) return false;
-    return true;
-  }
-
-  /**
-   * @generated
-   */
-  @Override
-  public int hashCode() {
-    int result = 1;
-    result = 31 * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
+    /**
+    * @generated
+    */
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
 }
--- project/W/cronapp-rad-project/src/main/java/app/entity/Securable.java
+++ project/W/cronapp-rad-project/src/main/java/app/entity/Securable.java
@@ -7,161 +7,112 @@ import javax.xml.bind.annotation.*;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import cronapi.rest.security.CronappSecurity;
+import org.eclipse.persistence.annotations.Convert;
+import org.eclipse.persistence.annotations.Converter;
+import cronapi.database.VersionConverter;
 
 
 /**
- * Classe que representa a tabela securable
- * @generated
- */
+* Classe que representa a tabela securable
+* @generated
+*/
 @Entity
 @Table(name = "\"securable\"")
 @XmlRootElement
 @CronappSecurity(post = "Administrators", get = "Administrators", delete = "Administrators", put = "Administrators")
 @JsonFilter("app.entity.Securable")
+@Converter(
+  name="version",
+  converterClass=VersionConverter.class
+)
 public class Securable implements Serializable {
 
-  /**
-   * UID da classe, necessário na serialização
-   * @generated
-   */
-  private static final long serialVersionUID = 1L;
+    /**
+    * UID da classe, necessário na serialização
+    * @generated
+    */
+    private static final long serialVersionUID = 1L;
 
-  /**
-   * @generated
-   */
-  @Id
-  @Column(name = "id", nullable = false, length=255, insertable=true, updatable=true)
-  private java.lang.String id;
+    /**
+    * @generated
+    */
+    @Id
+    @Column(name = "id", nullable = false, length=255, insertable=true, updatable=true)
+    private java.lang.String id;
 
-  /**
-  * @generated
-  */
-  @Column(name = "name", nullable = false, unique = false, length=255, insertable=true, updatable=true)
-  
-  private java.lang.String name;
+    /**
+    * @generated
+    */
+    @Column(name = "name", nullable = false, unique = false, length=255, insertable=true, updatable=true)
+    
+    private java.lang.String name;
 
-  /**
-  * @generated
-  */
-  @Column(name = "pattern", nullable = false, unique = false, length=255, insertable=true, updatable=true)
-  
-  private java.lang.String pattern;
+    /**
+    * Construtor
+    * @generated
+    */
+    public Securable(){
+    }
 
-  /**
-  * @generated
-  */
-  @Column(name = "type", nullable = false, unique = false, length=255, insertable=true, updatable=true)
-  
-  private java.lang.String type;
+    /**
+    * Obtém id
+    * return id
+    * @generated
+    */
+    
+    public java.lang.String getId(){
+        return this.id;
+    }
 
-  /**
-   * Construtor
-   * @generated
-   */
-  public Securable(){
-  }
+    /**
+    * Define id
+    * @param id id
+    * @generated
+    */
+    public Securable setId(java.lang.String id){
+        this.id = id;
+        return this;
+    }
+    /**
+    * Obtém name
+    * return name
+    * @generated
+    */
+    
+    public java.lang.String getName(){
+        return this.name;
+    }
 
+    /**
+    * Define name
+    * @param name name
+    * @generated
+    */
+    public Securable setName(java.lang.String name){
+        this.name = name;
+        return this;
+    }
 
-  /**
-   * Obtém id
-   * return id
-   * @generated
-   */
-  
-  public java.lang.String getId(){
-    return this.id;
-  }
+    /**
+    * @generated
+    */
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Securable object = (Securable)obj;
+        if (id != null ? !id.equals(object.id) : object.id != null) return false;
+        return true;
+    }
 
-  /**
-   * Define id
-   * @param id id
-   * @generated
-   */
-  public Securable setId(java.lang.String id){
-    this.id = id;
-    return this;
-  }
-
-  /**
-   * Obtém name
-   * return name
-   * @generated
-   */
-  
-  public java.lang.String getName(){
-    return this.name;
-  }
-
-  /**
-   * Define name
-   * @param name name
-   * @generated
-   */
-  public Securable setName(java.lang.String name){
-    this.name = name;
-    return this;
-  }
-
-  /**
-   * Obtém pattern
-   * return pattern
-   * @generated
-   */
-  
-  public java.lang.String getPattern(){
-    return this.pattern;
-  }
-
-  /**
-   * Define pattern
-   * @param pattern pattern
-   * @generated
-   */
-  public Securable setPattern(java.lang.String pattern){
-    this.pattern = pattern;
-    return this;
-  }
-
-  /**
-   * Obtém type
-   * return type
-   * @generated
-   */
-  
-  public java.lang.String getType(){
-    return this.type;
-  }
-
-  /**
-   * Define type
-   * @param type type
-   * @generated
-   */
-  public Securable setType(java.lang.String type){
-    this.type = type;
-    return this;
-  }
-
-  /**
-   * @generated
-   */
-  @Override
-  public boolean equals(Object obj) {
-    if (this == obj) return true;
-    if (obj == null || getClass() != obj.getClass()) return false;
-    Securable object = (Securable)obj;
-    if (id != null ? !id.equals(object.id) : object.id != null) return false;
-    return true;
-  }
-
-  /**
-   * @generated
-   */
-  @Override
-  public int hashCode() {
-    int result = 1;
-    result = 31 * result + ((id == null) ? 0 : id.hashCode());
-    return result;
-  }
+    /**
+    * @generated
+    */
+    @Override
+    public int hashCode() {
+        int result = 1;
+        result = 31 * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
 
 }
