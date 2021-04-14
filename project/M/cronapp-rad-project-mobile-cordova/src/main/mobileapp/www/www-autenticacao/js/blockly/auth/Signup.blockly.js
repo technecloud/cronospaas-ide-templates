7038abad739da647794b5fedd9c530fdbc7b57ca
Alisson Silva <alisson.silva@cronapp.io>
2021-04-14 11:11:50 -0300

    CRONAPP-3345 - Frontend Signup
    https://cronapp.atlassian.net/browse/CRONAPP-3345
    
    ### Problema:
    Erro pacote do bloco servidor
    ### Solução:
    Ajuste no pacote do bloco servidor

--- project/M/cronapp-rad-project-mobile-cordova/src/main/mobileapp/www/www-autenticacao/js/blockly/auth/Signup.blockly.js
+++ project/M/cronapp-rad-project-mobile-cordova/src/main/mobileapp/www/www-autenticacao/js/blockly/auth/Signup.blockly.js
@@ -1,16 +1,17 @@
 window.blockly = window.blockly || {};
 window.blockly.js = window.blockly.js || {};
 window.blockly.js.blockly = window.blockly.js.blockly || {};
-window.blockly.js.blockly.Signup = window.blockly.js.blockly.Signup || {};
+window.blockly.js.blockly.auth = window.blockly.js.blockly.auth || {};
+window.blockly.js.blockly.auth.Signup = window.blockly.js.blockly.auth.Signup || {};
 
 /**
  * Signup
  */
-window.blockly.js.blockly.auth.Signup.SignupArgs = ['signupUsername', 'signupEmail', 'signupPassword', 'signupConfirmPassword'];
+window.blockly.js.blockly.auth.Signup.SignupArgs = [{ description: 'signupUsername', id: '8df902ea' }, { description: 'signupEmail', id: '0c25d725' }, { description: 'signupPassword', id: '9c203fbf' }, { description: 'signupConfirmPassword', id: '067b073b' }];
 window.blockly.js.blockly.auth.Signup.Signup = async function(signupUsername, signupEmail, signupPassword, signupConfirmPassword) {
  var item;
-  if (await this.blockly.js.blockly.Signup.isValidSignup(signupUsername, signupEmail, signupPassword, signupConfirmPassword)) {
-    this.cronapi.util.callServerBlocklyAsynchronous('blockly.SignupApp:signupApp', async function(sender_item) {
+  if (await this.blockly.js.blockly.auth.Signup.isValidSignup(signupUsername, signupEmail, signupPassword, signupConfirmPassword)) {
+    this.cronapi.util.callServerBlocklyAsynchronous('blockly.Signup:signupApp', async function(sender_item) {
         item = sender_item;
       this.cronapi.screen.changeView("#/home/login",[  ]);
     }.bind(this), signupUsername, signupEmail, signupPassword);
@@ -20,7 +21,7 @@ window.blockly.js.blockly.auth.Signup.Signup = async function(signupUsername, si
 /**
  * Descreva esta função...
  */
-window.blockly.js.blockly.auth.Signup.isValidSignupArgs = ['signupUsername', 'signupEmail', 'signupPassword', 'signupConfirmPassword'];
+window.blockly.js.blockly.auth.Signup.isValidSignupArgs = [{ description: 'signupUsername', id: 'be2325de' }, { description: 'signupEmail', id: 'dc667925' }, { description: 'signupPassword', id: '9a49950f' }, { description: 'signupConfirmPassword', id: 'f6fdc12c' }];
 window.blockly.js.blockly.auth.Signup.isValidSignup = async function(signupUsername, signupEmail, signupPassword, signupConfirmPassword) {
  var item;
   isValid = true;
