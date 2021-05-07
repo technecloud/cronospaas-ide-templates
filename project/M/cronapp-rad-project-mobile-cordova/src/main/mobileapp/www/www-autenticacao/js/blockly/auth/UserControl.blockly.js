ee598a006901b1eb8f6d87cd691fdd561e695046
Alisson Silva <alisson.silva@cronapp.io>
2021-05-07 15:37:27 -0300

    CRONAPP-3476 - Ajuste Signup e Reset Password
    https://cronapp.atlassian.net/browse/CRONAPP-3329
    
    Problema:
    Não existia bloco para reset password
    
    Solução:
    Desenvolvido bloco para o reset password

--- project/M/cronapp-rad-project-mobile-cordova/src/main/mobileapp/www/www-autenticacao/js/blockly/auth/UserControl.blockly.js
+++ project/M/cronapp-rad-project-mobile-cordova/src/main/mobileapp/www/www-autenticacao/js/blockly/auth/UserControl.blockly.js
@@ -9,7 +9,7 @@ window.blockly.js.blockly.auth.UserControl = window.blockly.js.blockly.auth.User
  */
 window.blockly.js.blockly.auth.UserControl.signUpArgs = [{ description: 'signupUsername', id: '8df902ea' }, { description: 'signupEmail', id: '0c25d725' }, { description: 'signupPassword', id: '9c203fbf' }, { description: 'signupConfirmPassword', id: '067b073b' }];
 window.blockly.js.blockly.auth.UserControl.signUp = async function(signupUsername, signupEmail, signupPassword, signupConfirmPassword) {
- var returnJson, httpStatus;
+
   if (await this.blockly.js.blockly.auth.UserControl.isValidSignup(signupUsername, signupEmail, signupPassword, signupConfirmPassword)) {
     this.cronapi.util.callServerBlocklyAsynchronous('blockly.UserControl:signUp', async function(sender_returnJson) {
         returnJson = sender_returnJson;
@@ -29,13 +29,14 @@ window.blockly.js.blockly.auth.UserControl.signUp = async function(signupUsernam
  */
 window.blockly.js.blockly.auth.UserControl.resetPasswordArgs = [{ description: 'email', id: '7b9f3909' }];
 window.blockly.js.blockly.auth.UserControl.resetPassword = async function(email) {
- var signupUsername, signupEmail, signupPassword, signupConfirmPassword, returnJson;
+ var item;
   if (this.cronapi.logic.isNullOrEmpty(email)) {
     this.cronapi.screen.notify('error',this.cronapi.i18n.translate("UsernameCanNotBeEmpty",[  ]));
   } else {
     this.cronapi.util.callServerBlocklyAsynchronous('blockly.UserControl:resetPassword', async function(sender_item) {
         item = sender_item;
       this.cronapi.screen.notify('success',this.cronapi.i18n.translate("ForgotPasswordSent",[  ]));
+      this.cronapi.screen.changeView("#/app/login",[  ]);
     }.bind(this), email);
   }
 }
@@ -45,7 +46,7 @@ window.blockly.js.blockly.auth.UserControl.resetPassword = async function(email)
  */
 window.blockly.js.blockly.auth.UserControl.isValidSignupArgs = [{ description: 'signupUsername', id: 'be2325de' }, { description: 'signupEmail', id: 'dc667925' }, { description: 'signupPassword', id: '9a49950f' }, { description: 'signupConfirmPassword', id: 'f6fdc12c' }];
 window.blockly.js.blockly.auth.UserControl.isValidSignup = async function(signupUsername, signupEmail, signupPassword, signupConfirmPassword) {
- var returnJson, httpStatus;
+
   isValid = true;
   if (this.cronapi.logic.isNullOrEmpty(signupUsername)) {
     this.cronapi.screen.notify('error',this.cronapi.i18n.translate("UsernameCanNotBeEmpty",[  ]));
--- project/W/cronapp-rad-project/src/main/webapp-autenticacao/webapp/js/blockly/UserControl.blockly.js
+++ project/W/cronapp-rad-project/src/main/webapp-autenticacao/webapp/js/blockly/UserControl.blockly.js
@@ -8,13 +8,14 @@ window.blockly.js.blockly.UserControl = window.blockly.js.blockly.UserControl ||
  */
 window.blockly.js.blockly.UserControl.resetPasswordArgs = [{ description: 'email', id: '7afdf898' }];
 window.blockly.js.blockly.UserControl.resetPassword = async function(email) {
- var signupUsername, signupEmail, signupPassword, signupConfirmPassword, returnJson;
+ var item;
   if (this.cronapi.logic.isNullOrEmpty(email)) {
     this.cronapi.screen.notify('error',this.cronapi.i18n.translate("EmailCanNotBeEmpty",[  ]));
   } else {
     this.cronapi.util.callServerBlocklyAsynchronous('blockly.UserControl:resetPassword', async function(sender_item) {
         item = sender_item;
       this.cronapi.screen.notify('success',this.cronapi.i18n.translate("ForgotPasswordSent",[  ]));
+      this.cronapi.screen.changeView("#/app/login",[  ]);
     }.bind(this), email);
   }
 }
@@ -24,11 +25,10 @@ window.blockly.js.blockly.UserControl.resetPassword = async function(email) {
  */
 window.blockly.js.blockly.UserControl.signUpArgs = [{ description: 'signupUsername', id: 'ec5dbe32' }, { description: 'signupEmail', id: '62cce53e' }, { description: 'signupPassword', id: 'd42229ad' }, { description: 'signupConfirmPassword', id: 'a49023f3' }];
 window.blockly.js.blockly.UserControl.signUp = async function(signupUsername, signupEmail, signupPassword, signupConfirmPassword) {
- var returnJson, httpStatus;
+
   if (await this.blockly.js.blockly.UserControl.isValidSignup(signupUsername, signupEmail, signupPassword, signupConfirmPassword)) {
     this.cronapi.util.callServerBlocklyAsynchronous('blockly.UserControl:signUp', async function(sender_returnJson) {
         returnJson = sender_returnJson;
-      console.log(returnJson);
       httpStatus = this.cronapi.json.getProperty(this.cronapi.json.createObjectFromString(returnJson), 'code');
       if (httpStatus == '200') {
         this.cronapi.screen.notify('success',this.cronapi.i18n.translate("UserSuccessfullyRegistered",[  ]));
@@ -45,7 +45,7 @@ window.blockly.js.blockly.UserControl.signUp = async function(signupUsername, si
  */
 window.blockly.js.blockly.UserControl.isValidSignupArgs = [{ description: 'signupUsername', id: 'abf7b641' }, { description: 'signupEmail', id: '38708282' }, { description: 'signupPassword', id: 'daf1486e' }, { description: 'signupConfirmPassword', id: '3f9f5d23' }];
 window.blockly.js.blockly.UserControl.isValidSignup = async function(signupUsername, signupEmail, signupPassword, signupConfirmPassword) {
- var returnJson, httpStatus;
+
   isValid = true;
   if (this.cronapi.logic.isNullOrEmpty(signupUsername)) {
     this.cronapi.screen.notify('error',this.cronapi.i18n.translate("UsernameCanNotBeEmpty",[  ]));
